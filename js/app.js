26fd1d3959d16c2e0e7dc6d8cc98f60cdb6ee33b
eze210 <eze210@gmail.com>
2018-09-09 21:20:55 -0300

    Adding timeout route to javascript app.

--- js/app.js
+++ js/app.js
@@ -4,8 +4,16 @@ const app = express();
 
 const PORT = 5000
 
+const TIMEOUT_SECONDS = 5
+
 app.get("/", (req, res) => {
 	res.send("Hola, mundo!");
+});
+
+app.get("/timeout", (req, res) => {
+	setTimeout(() => {
+		res.send("Timeout reached");
+	}, TIMEOUT_SECONDS * 1000);
 });
 
 app.listen(PORT, () => {
