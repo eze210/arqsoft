8feb43c84455c5b98dc35bfa5904429327f6b51a
Seva Gavrilov <gavrilovseva@gmail.com>
2018-09-30 23:28:11 -0300

    Added some new endpoint to services, and some logs.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-const express = require("express");
+const express = require('express');
+const https = require('https');
 
 const app = express();
 
@@ -6,16 +7,39 @@ const PORT = 3000;
 
 const TIMEOUT_SECONDS = 5;
 
-app.get("/", (req, res) => {
-	res.send("Hola, mundo!");
+app.get('/', (req, res) => {
+  console.log('Requested /, saying "Hola, mundo"');
+	res.send('Hola, mundo!');
 });
 
-app.get("/timeout", (req, res) => {
+app.get('/timeout', (req, res) => {
+  console.log('Requested /timeout, sleeping', TIMEOUT_SECONDS, 'seconds');
 	setTimeout(() => {
-		res.send("Timeout reached");
+		res.send('Timeout reached');
 	}, TIMEOUT_SECONDS * 1000);
 });
 
+app.get('/cpu', (req, res) => {
+  console.log('Requested /cpu, using cpu for 5 seconds');
+  var start = new Date().getTime();
+
+  var result = 0
+  while(new Date().getTime() - start < 5000) {
+    result += Math.random() * Math.random();
+  }
+
+  res.send('Finished using cpu!');
+});
+
+app.get('/external', (req, res) => {
+  var url = 'https://httpstat.us/200?sleep=5000';
+  console.log('Requested /external endpoint, making a request to ' + url);
+  https.get(url, (response) => {
+    const { statusCode } = response;
+    res.send('External service request finished with status: ' + statusCode);
+  });
+});
+
 app.listen(PORT, () => {
-	console.log("Listenning on port", PORT);
+	console.log('Listenning on port', PORT);
 });
--- py/app.py
+++ py/app.py
@@ -1,18 +1,42 @@
-from flask import Flask
 import time
+import logging
+from flask import Flask
+import urllib.request
 
 app = Flask(__name__)
+app.logger.setLevel(logging.INFO)
 
 
-@app.route("/")
+@app.route('/')
 def root():
-    return "Hola, mundo!"
+    app.logger.info('Requested /, saying "Hola, mundo"')
+    return 'Hola, mundo!'
 
 
-@app.route("/timeout")
+@app.route('/timeout')
 def timeout():
+    app.logger.info('Requested /timeout, sleeping 5 seconds')
     time.sleep(5)
-    return "Timeout reached at: {}".format(time.time())
+    return 'Timeout reached at: {}'.format(time.time())
+
+
+@app.route('/cpu')
+def cpu_usage():
+    app.logger.info('Requested /cpu, using cpu for 5 seconds')
+    start = time.time()
+
+    while time.time() - start < 5:
+        987239478234879 * 98723947823947
+
+    return 'Finished using cpu!'
+
+
+@app.route('/external')
+def external_request():
+    url = 'https://httpstat.us/200?sleep=5000'
+    app.logger.info('Requested /external, making a request to ' + url)
+    response = urllib.request.urlopen(url)
+    return 'External service request finished with status: ' + str(response.status)
 
 
 if __name__ == '__main__':
